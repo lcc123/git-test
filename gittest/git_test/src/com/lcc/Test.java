e05f011944a52d497ca26c3e6f08bdd4a51bec79
lcc123 <13760950216@163.com>
2019-10-12 12:24:55 +0800

    first commit

---
+++ gittest/git_test/src/com/lcc/Test.java
@@ -0,0 +1,11 @@
+package com.lcc;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+        System.out.println("hello world!!!");
+        System.out.println("hello world!!!");
+        System.out.println("hello world!!!");
+    }
+}
