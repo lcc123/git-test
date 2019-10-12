e70c38d16aa0b9aa263c9b30fe008e3c5e77c54b
lcc123 <13760950216@163.com>
2019-10-12 13:03:09 +0800

    修改了内容

--- gittest/git_test/src/com/lcc/Test.java
+++ gittest/git_test/src/com/lcc/Test.java
@@ -8,10 +8,11 @@ public class Test {
         System.out.println("hello world!!!");
         System.out.println("hello world!!!");
         test();
+        // 测试
     }
     public static  void test(){
         int i=0 ;
         i += 10;
-
+        System.out.println("i 的数字为"+i);
     }
 }
